776c85cbf080077346d673bdc22db1de46a4fd72
THANIGAIMANIKUMARAN <43335182+THANIGAIMANIKUMARAN@users.noreply.github.com>
2018-12-03 10:42:23 +0530

    Add files via upload

---
+++ WebContent/services/blogservice.js
@@ -0,0 +1,56 @@
+/**
+ * BlogService
+ */
+app.factory('BlogService',function($http){
+	var blogService={}
+	
+	blogService.addBlog=function(blog){
+		return $http.post("http://localhost:8080/middleware/addblogpost",blog)
+	}
+	
+	blogService.getBlogsWaitingForApproval=function(){
+		return $http.get("http://localhost:8080/middleware/blogswaitingforapproval")
+	}
+	
+	blogService.getBlogsApproved=function(){
+		return $http.get("http://localhost:8080/middleware/blogsapproved")
+	}
+	
+	blogService.getBlog=function(blogPostId){
+		return $http.get("http://localhost:8080/middleware/getBlog/"+blogPostId)
+	}
+	
+    blogService.approve=function(blogPost){
+    	return $http.put("http://localhost:8080/middleware/approve",blogPost)
+    }	
+	
+	blogService.reject=function(blogPost,rejectionReason){
+		return $http.put("http://localhost:8080/middleware/reject/"+rejectionReason,blogPost)
+	}
+	
+	blogService.hasUserLikedBlogPost=function(blogPostId){
+		return $http.get("http://localhost:8080/middleware/hasUserLikedBlogPost/"+blogPostId);
+	}
+	
+	blogService.updateLikes=function(blogPostId){
+		return $http.put("http://localhost:8080/middleware/updatelikes/"+blogPostId);
+	}
+	
+	blogService.addBlogComment=function(blogComment){
+		//newly created blogcomment object with the values for two properties - blogPost, commentTxt
+		//blogComment {'blogPost':{},'commentTxt':'Thanks'}
+		return $http.post("http://localhost:8080/middleware/addblogcomment",blogComment)
+	}
+	
+	blogService.getAllBlogComments=function(blogPostId){
+	return $http.get("http://localhost:8080/middleware/getblogcomments/"+blogPostId)	
+	}
+	
+	return blogService;
+})
+
+
+
+
+
+
---
+++ WebContent/services/userservice.js
@@ -0,0 +1,23 @@
+/**
+	 * UserService
+	 */
+	app.factory('UserService',function($http){
+		var userService={}
+		
+		userService.registration=function(user){//user from controller
+			return $http.post("http://localhost:8080/middleware/register",user)
+		}
+		
+		userService.login=function(user){
+			return $http.put("http://localhost:8080/middleware/login",user)
+		}
+		
+		
+		userService.logout=function(){
+			return $http.put("http://localhost:8080/middleware/logout")
+		}
+		
+		return userService;
+	})
+
+
